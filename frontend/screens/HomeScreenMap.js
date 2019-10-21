872afff3d3c886c3528533722dca36326bb71db2
eddymaric <eddy.maric98@gmail.com>
2019-10-20 20:37:13 -0700

    initial Firebase setup
    App is set up to recieve firebase notifications.

--- frontend/screens/HomeScreenMap.js
+++ frontend/screens/HomeScreenMap.js
@@ -11,6 +11,7 @@ import {
   StyleSheet,
   ScrollView,
   View,
+  Alert,
 } from 'react-native';
 
 import {
@@ -25,6 +26,8 @@ import tabBarIcon from '../components/tabBarIcon';
 import AddListingPage from '../components/AddListingPage';
 import Listing from '../classes/Listing';
 import { DB_URL } from '../key';
+import { AsyncStorage } from 'react-native';
+import firebase from 'react-native-firebase';
 
 export default class HomeScreenMap extends React.Component {
   static navigationOptions = {
@@ -54,8 +57,94 @@ export default class HomeScreenMap extends React.Component {
   }
 
   componentDidMount(){
+    this.checkPermission();
+    this.createNotificationListeners();
     this.getListings();
   }
+
+  async checkPermission() {
+  const enabled = await firebase.messaging().hasPermission();
+  if (enabled) {
+      this.getToken();
+  } else {
+      this.requestPermission();
+  }
+}
+
+async getToken() {
+  let fcmToken = await AsyncStorage.getItem('fcmToken');
+  if (!fcmToken) {
+      fcmToken = await firebase.messaging().getToken();
+      if (fcmToken) {
+          // user has a device token
+          await AsyncStorage.setItem('fcmToken', fcmToken);
+      }
+  }
+
+  alert(fcmToken);
+}
+
+async requestPermission() {
+  try {
+      await firebase.messaging().requestPermission();
+      // User has authorised
+      console.log('permission granted');
+      this.getToken();
+  } catch (error) {
+      // User has rejected permissions
+      console.log('permission rejected');
+  }
+}
+
+//Remove listeners allocated in createNotificationListeners()
+componentWillUnmount() {
+this.notificationListener();
+this.notificationOpenedListener();
+}
+
+async createNotificationListeners() {
+/*
+* Triggered when a particular notification has been received in foreground
+* */
+this.notificationListener = firebase.notifications().onNotification((notification) => {
+    const { title, body } = notification;
+    this.showAlert(title, body);
+});
+
+/*
+* If your app is in background, you can listen for when a notification is clicked / tapped / opened as follows:
+* */
+this.notificationOpenedListener = firebase.notifications().onNotificationOpened((notificationOpen) => {
+    const { title, body } = notificationOpen.notification;
+    this.showAlert(title, body);
+});
+
+/*
+* If your app is closed, you can check if it was opened by a notification being clicked / tapped / opened as follows:
+* */
+const notificationOpen = await firebase.notifications().getInitialNotification();
+if (notificationOpen) {
+    const { title, body } = notificationOpen.notification;
+    this.showAlert(title, body);
+}
+/*
+* Triggered for data only payload in foreground
+* */
+this.messageListener = firebase.messaging().onMessage((message) => {
+  //process data message
+  console.log(JSON.stringify(message));
+});
+}
+
+showAlert(title, body) {
+Alert.alert(
+  title, body,
+  [
+      { text: 'OK', onPress: () => console.log('OK Pressed') },
+  ],
+  { cancelable: false },
+);
+}
 
   addLocalMarker = (listing) => {
     this.setState({
