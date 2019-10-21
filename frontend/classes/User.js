db15ddeed2de56de1091fd91ad0204ac91db489d
mickeyneuf <mikayla.a.neufeld@gmail.com>
2019-10-21 15:23:05 -0700

    Fixed schema name

--- frontend/classes/User.js
+++ frontend/classes/User.js
@@ -1,6 +1,6 @@
 export default class User {
   constructor(json) {
-    this.user_name = json["user_name"];
-    this.user_email = json["user_email"];
+    this.name = json["name"];
+    this.email = json["email"];
   }
 }
