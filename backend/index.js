9668b1a1d6b9937405a642fe549c2156f881f274
Ubuntu <renteradmin@ubuntuServerCPEN321.wus43hvydz3etpnxxaiupw3acf.dx.internal.cloudapp.net>
2019-10-21 00:28:02 -0700

    Backend - added endpoints for logging in and signing up

--- backend/index.js
+++ backend/index.js
@@ -7,31 +7,40 @@ var jsonParser = bodyParser.json();
 
 mongoclient.connect("mongodb://0.0.0.0:27017",(err,client)=> {db = client.db('redb')});
 
-app.post('/sign_up', jsonParser, (req,res) => {
-	console.log("Sign up");
-	db.collection("users").insertOne(req.body, (err, result) => {
-		if(err){
+app.post('/signup', jsonParser, (req,res) => {
+	console.log(req.body);
+	db.collection("users").find({"email":req.body.email}).count(function (err, count){
+		if(err) {
 			res.sendStatus(400);
-		} else {
-			res.sendStatus(200);
+		}
+		else{
+			if (count == 0) {
+				db.collection("users").insertOne(req.body, (err, result) => {
+					if(err) {
+						res.sendStatus(400);
+					} else {
+						res.sendStatus(201);
+					}
+				});
+			} else {
+				res.sendStatus(401);
+			}
 		}
 	});
 });
 
-app.get('/login', (req,res) => {
-	console.log("Enter login endpoint.");
-	console.log(req.query.id);
-	console.log(req.query.password);
-	db.collection("users").find({"user_id":req.query.id, "password":req.query.password}, (err, result) => {
-		if(err){
+app.post('/login', jsonParser, (req,res) => {
+	console.log(req.body);
+	db.collection("users").find(req.body).count(function (err, count){
+		console.log(count);
+		if(err) {
 			res.sendStatus(400);
-		}
-		if(result){
-			res.sendStatus(200);
-		}
-		else{
+		} else if (count > 0) {
+			res.sendStatus(201);
+		} else {
 			res.sendStatus(401);
 		}
+
 	});
 });
 
@@ -39,8 +48,6 @@ app.get('/get_listings', jsonParser, (req, res) => {
 	console.log("GETTING LISTING WITH QUERY: ");
 	console.log(req.body);
 	db.collection("listings").find(req.body).toArray((err,result) => {
-		console.log("SENT: ")
-		console.log(result);
 		res.send(result);
 	});
 });
