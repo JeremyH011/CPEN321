377ade774a36dbe8016c7a24436c59a30e257843
victor-z-xiong <victorzxiong98@gmail.com>
2019-10-25 23:01:04 -0700

    issue #3: change default location to vancouver from san fran

--- frontend/components/UsersMap.js
+++ frontend/components/UsersMap.js
@@ -31,8 +31,8 @@ export default class UserMap extends PureComponent {
         <View style={styles.mapContainer}>
             <MapView
             initialRegion={{
-                latitude: 37.78825,
-                longitude: -122.4324,
+                latitude: 49.2827,
+                longitude: -123.1207,
                 latitudeDelta: 0.0622,
                 longitudeDelta: 0.0421,
             }}
