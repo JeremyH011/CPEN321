f5be999bae497fbe2d675a7163f5c845e18399cd
Mikayla Neufeld <mickeyneuf@gmail.com>
2019-10-31 14:56:36 -0700

    Inital changes (committing to rebase with updated signin functionality)

---
+++ frontend/components/ViewUserPage.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import {Modal, Text} from 'react-native';
+
+export default class ViewUserPage extends React.Component {
+
+  render() {
+      return (
+        <Modal
+          visible = {this.props.visible}
+          onRequestClose = {this.props.close}
+          style = {styles.modal}
+        >
+        <View style={styles.profilePic}>
+          <Image source={require('./Profile_Placeholder.png')} />
+        </View>
+          <Text>Name : {this.props.name}</Text>
+          <TouchableOpacity style={styles.modalButton} onPress={() => this.props.navigation.navigate("Chat") }>
+            <Text>Chat</Text>
+          </TouchableOpacity>
+        </Modal>
+      );
+  }
+}
+
+const styles = StyleSheet.create({
+  modal: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  modalButton: {
+    alignItems: 'center',
+    backgroundColor: '#DDDDDD',
+    margin: 10,
+    padding: 10,
+  },
+  profilePic: {
+    width: 300,
+    height: 300,
+    justifyContent: 'center',
+  }
+});
