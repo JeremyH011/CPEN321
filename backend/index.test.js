e9534bd97a2ad24a765d95dbfff22801bcb1fe5f
Jeremy Hidjaja <jeremyh1101@gmail.com>
2019-11-16 17:07:53 -0800

    Travis sample test

--- backend/index.test.js
+++ backend/index.test.js
@@ -2,10 +2,19 @@ const app = require('./app'); // Link to your server file
 const supertest = require('supertest');
 const request = supertest(app);
 
-it('gets the test endpoint', async done => {
+it('GETS the test endpoint', async done => {
   const response = await request.get('/test')
 
   expect(response.status).toBe(200);
   expect(response.body.message).toBe('pass!');
+  done();
+});
+
+it('POST new user', async done =>{
+  body = {email: 'test_email@test.com', password: 'test'};
+  const response = await request.post('/signup')
+                                .send(body).
+                                .set('Accept','application/json');
+  expect(response.status).toBe(201);
   done();
 });
