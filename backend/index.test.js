76a176c68a8c43176077a9976a0e3ebadb22d06e
Jeremy Hidjaja <jeremyh1101@gmail.com>
2019-11-16 16:25:18 -0800

    Travis sample test

--- backend/index.test.js
+++ backend/index.test.js
@@ -5,7 +5,7 @@ const request = supertest(app);
 it('gets the test endpoint', async done => {
   const response = await request.get('/test')
 
-  expect(response.status).toBe(200)
-  expect(response.body.message).toBe('pass!')
-  done()
+  expect(response.status).toBe(200);
+  expect(response.body.message).toBe('pass!');
+  done();
 });
